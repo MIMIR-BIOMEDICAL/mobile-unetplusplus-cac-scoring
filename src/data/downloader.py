f7c9d5ef637c8db6b2dbecb90e6fdc5a21846f87
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2022-12-07 22:37:16 +0700

    chores: added downloader docs

--- src/data/downloader.py
+++ src/data/downloader.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 @click.command()
-@click.option("-u", "--url", help="Dataset URL")
+@click.option("-u", "--url", type=click.STRING, help="Dataset URL")
 @click.option(
     "-d",
     "--dest",
@@ -16,13 +16,20 @@ from dotenv import load_dotenv
     help="Destination folder",
 )
 def download_dataset(url, dest):
+    """
+    A function (and cli) to download (specifically COCA Dataset)
 
+    The azure url can be added through environment variable "DATASET_URL"
+    or via the url variable
+
+    url (str): Dataset URL
+    dest (str): The parent folder for the downloaded dataset, default to "data/raw/"
+
+    """
     load_dotenv()
 
-    if which("azcopy") is None:
-        click.echo("AZCopy is not installed, installing azcopy...")
-        subprocess.run(["src/data/install_azcopy.sh"], text=True)
-        click.echo("AZCopy installed")
+    subprocess.run(["src/data/install_azcopy.sh"], text=True)
+    click.echo("AZCopy installed")
 
     if url is not None:
         subprocess.run(
