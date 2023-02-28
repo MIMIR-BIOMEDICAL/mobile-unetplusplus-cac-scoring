507a55151688248c4aecb9346da85e209a144ac3
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-02-28 19:51:47 +0700

    feat: added example parser function

--- src/data/preprocess/lib/tfrecord.py
+++ src/data/preprocess/lib/tfrecord.py
@@ -78,3 +78,34 @@ def create_example_fn(combined_data_dict: dict):
         "mult_seg": sparse_coord_feature(combined_data_dict["mult_seg"]),
     }
     return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+def parsed_example_fn(example):
+    """
+    A function that parsed a serialize example into its original data
+    source
+
+    Args:
+        example ():
+
+    Returns:
+
+    """
+    feature_description = {
+        "patient_num": tf.io.FixedLenFeature([], tf.string),
+        "idx": tf.io.FixedLenFeature([], tf.string),
+        "img": tf.io.FixedLenFeature([], tf.string),
+        "bin_seg": tf.io.VarLenFeature(tf.int64),
+        "multi_seg": tf.io.VarLenFeature(tf.int64),
+    }
+    parsed_example = tf.io.parse_example(example, feature_description)
+    parsed_example["img"] = tf.io.parse_tensor(parsed_example["img"], tf.uint16)
+
+    dense_bin_seg = tf.sparse.to_dense(parsed_example["bin_seg"])
+    parsed_example["bin_seg"] = tf.reshape(dense_bin_seg, [len(dense_bin_seg) // 2, 2])
+
+    dense_multi_seg = tf.sparse.to_dense(parsed_example["multi_seg"])
+    parsed_example["mult_seg"] = tf.reshape(
+        dense_multi_seg, [len(dense_multi_seg) // 3, 3]
+    )
+    return parsed_example
