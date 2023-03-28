33c16e230e743756c98cd6d963e76778f84a577b
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2023-03-28 19:28:34 +0700

    feat: Add builder function as interface (#48)
    * feat: Add builder function as interface
    
    * style: Format code with black and isort
    
    ---------
    
    Co-authored-by: Aditya Wardianto <aditya.wardianto@zero-one-group.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ src/models/builder.py
@@ -0,0 +1,69 @@
+"""Builder module as interface to building model"""
+import pathlib
+import sys
+
+from tensorflow import keras  # pylint: disable=wrong-import-position,import-error
+
+sys.path.append(pathlib.Path.cwd().parent.as_posix())
+from src.models.base import base_unet_pp
+from src.models.config import UNetPPConfig
+
+
+def build_unet_pp(config: UNetPPConfig, custom: bool = False) -> keras.Model:
+    """
+    Builds a UNet++ model.
+
+    Args:
+        config: Configuration object specifying the parameters of the model.
+        custom: Whether to build a custom model or use the default parameters.
+
+    Returns:
+        A UNet++ model.
+
+    Raises:
+        ValueError: If the specified configuration is invalid.
+    """
+    if custom:
+        if config.model_mode == "basic":
+            if config.downsample_iteration is not None:
+                raise ValueError(
+                    "You are using basic mode, downsample_iteration is not required"
+                )
+        elif config.model_mode == "mobile":
+            if config.downsample_iteration is None:
+                raise ValueError(
+                    "You are using mobile mode, downsample_iteration is required"
+                )
+        else:
+            raise ValueError(f"Invalid model mode: {config.model_mode}")
+
+        return base_unet_pp(config)
+
+    if config.model_mode == "basic":
+        model_conf = UNetPPConfig(
+            input_dim=[512, 512, 1],
+            batch_norm=True,
+            model_mode="basic",
+            depth=5,
+            n_class={"bin": 1},
+            deep_supervision=True,
+            upsample_mode="transpose",
+            filter_list=[32, 64, 128, 256, 512],
+        )
+
+    elif config.model_mode == "mobile":
+        model_conf = UNetPPConfig(
+            upsample_mode="upsample",
+            depth=5,
+            input_dim=(512, 512, 1),
+            batch_norm=True,
+            model_mode="mobile",
+            n_class={"bin": 1, "mult": 4},
+            deep_supervision=True,
+            filter_list=[16, 32, 64, 128, 256],
+            downsample_iteration=[1, 2, 3, 3, 2],
+        )
+    else:
+        raise ValueError(f"Invalid model mode: {config.model_mode}")
+
+    return base_unet_pp(model_conf)
