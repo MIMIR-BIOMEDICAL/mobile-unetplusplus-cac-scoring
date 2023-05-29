003ac83abe092bc22bfec94ae055f62f9b4fcabe
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-05-29 14:18:26 +0700

    feat: aded dice loss derivated from tversky

--- src/models/lib/loss.py
+++ src/models/lib/loss.py
@@ -3,6 +3,32 @@ import tensorflow as tf
 from tensorflow.keras import backend as K
 
 
+# Ref: salehi17, "Twersky loss function for image segmentation using 3D FCDN"
+# -> the score is computed for each class separately and then summed
+# alpha=beta=0.5 : dice coefficient
+# alpha=beta=1   : tanimoto coefficient (also known as jaccard)
+# alpha+beta=1   : produces set of F*-scores
+# implemented by E. Moebel, 06/04/18
+def dice_loss_from_tversky(y_true, y_pred):
+    alpha = 0.5
+    beta = 0.5
+
+    ones = K.ones(K.shape(y_true))
+    p0 = y_pred  # proba that voxels are class i
+    p1 = ones - y_pred  # proba that voxels are not class i
+    g0 = y_true
+    g1 = ones - y_true
+
+    num = K.sum(p0 * g0, (0, 1, 2))
+    den = num + alpha * K.sum(p0 * g1, (0, 1, 2)) + beta * K.sum(p1 * g0, (0, 1, 2))
+
+    # when summing over classes, T has dynamic range [0 Ncl]
+    T = K.sum(num / den)
+
+    n_class = K.cast(K.shape(y_true)[-1], "float32")
+    return n_class - T
+
+
 def categorical_focal_loss(gamma=2.0, alpha=0.25):
     """
     Implementation of Focal Loss from the paper in multiclass classification
