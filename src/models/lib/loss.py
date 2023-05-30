715bcb135f0382d85c3ef5a3b4770be77069d49e
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-05-30 22:02:37 +0700

    fix: reimplement categorical focal loss

--- src/models/lib/loss.py
+++ src/models/lib/loss.py
@@ -29,39 +29,56 @@ def dice_loss_from_tversky(y_true, y_pred):
     n_class = K.cast(K.shape(y_true)[-1], "float32")
     return n_class - T
 
-
-def categorical_focal_loss(gamma=2.0, alpha=0.25):
+def categorical_focal_loss(alpha=0.25, gamma=2.):
     """
-    Implementation of Focal Loss from the paper in multiclass classification
-    Formula:
-        loss = -alpha*((1-p)^gamma)*log(p)
+    Softmax version of focal loss.
+    When there is a skew between different categories/labels in your data set, you can try to apply this function as a
+    loss.
+           m
+      FL = ∑  -alpha * (1 - p_o,c)^gamma * y_o,c * log(p_o,c)
+          c=1
+
+      where m = number of classes, c = class and o = observation
+
     Parameters:
-        alpha -- the same as wighting factor in balanced cross entropy
-        gamma -- focusing parameter for modulating factor (1-p)
+      alpha -- the same as weighing factor in balanced cross entropy. Alpha is used to specify the weight of different
+      categories/labels, the size of the array needs to be consistent with the number of classes.
+      gamma -- focusing parameter for modulating factor (1-p)
+
     Default value:
-        gamma -- 2.0 as mentioned in the paper
-        alpha -- 0.25 as mentioned in the paper
+      gamma -- 2.0 as mentioned in the paper
+      alpha -- 0.25 as mentioned in the paper
+
+    References:
+        Official paper: https://arxiv.org/pdf/1708.02002.pdf
+        https://www.tensorflow.org/api_docs/python/tf/keras/backend/categorical_crossentropy
+
+    Usage:
+     model.compile(loss=[categorical_focal_loss(alpha=[[.25, .25, .25]], gamma=2)], metrics=["accuracy"], optimizer=adam)
     """
 
-    def focal_loss(y_true, y_pred):
-        # Define epsilon so that the backpropagation will not result in NaN
-        # for 0 divisor case
-        epsilon = K.epsilon()
-        # Add the epsilon to prediction value
-        # y_pred = y_pred + epsilon
-        # Clip the prediction value
-        y_pred = K.clip(y_pred, epsilon, 1.0 - epsilon)
-        # Calculate cross entropy
-        cross_entropy = -y_true * K.log(y_pred)
-        # Calculate weight that consists of  modulating factor and weighting factor
-        weight = alpha * y_true * K.pow((1 - y_pred), gamma)
-        # Calculate focal loss
-        loss = weight * cross_entropy
-        # Sum the losses in mini_batch
-        loss = K.sum(loss, axis=1)
-        return loss
 
-    return focal_loss
+    def categorical_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred: A tensor resulting from a softmax
+        :return: Output tensor.
+        """
+
+        # Clip the prediction value to prevent NaN's and Inf's
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+
+        # Calculate Cross Entropy
+        cross_entropy = -y_true * K.log(y_pred)
+
+        # Calculate Focal Loss
+        loss = alpha * K.pow(1 - y_pred, gamma) * cross_entropy
+
+        # Compute mean loss in mini_batch
+        return K.mean(K.sum(loss, axis=-1))
+
+    return categorical_focal_loss_fixed
 
 
 # Helper function to enable loss function to be flexibly used for
