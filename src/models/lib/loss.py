3294000057baa1c1e782fc36bec88a55c4308062
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-06-03 18:24:00 +0700

    feat: added weighted categorical crossentropy

--- src/models/lib/loss.py
+++ src/models/lib/loss.py
@@ -83,3 +83,31 @@ def dice_loss_func(y_true, y_pred):
 def log_cosh_dice_loss(y_true, y_pred):
     dice_loss = dice_loss_func(y_true, y_pred)
     return tf.math.log((tf.exp(dice_loss) + tf.exp(-dice_loss)) / 2.0)
+
+
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+
+    weights = K.variable(weights)
+
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+
+    return loss
