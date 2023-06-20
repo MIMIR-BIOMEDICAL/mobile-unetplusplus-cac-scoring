a2062a9372d3df58c84f55a7ad14654a255d5eec
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-06-20 21:20:00 +0700

    fix: add missing concat layer name

--- src/models/lib/base.py
+++ src/models/lib/base.py
@@ -2,23 +2,19 @@
 import pathlib
 import sys
 
-from tensorflow import keras  # pylint: disable=wrong-import-position,import-error
-from tensorflow.keras import (  # pylint: disable=wrong-import-position,import-error
-    layers,
-)
+from tensorflow import \
+    keras  # pylint: disable=wrong-import-position,import-error
+from tensorflow.keras import \
+    layers  # pylint: disable=wrong-import-position,import-error
 from tensorflow.keras.applications import MobileNetV2
 
 sys.path.append(pathlib.Path.cwd().as_posix())
 
 from src.models.lib.block import (  # pylint: disable=wrong-import-position,import-error
-    conv_bn_relu_block,
-    sequence_inv_res_bot_block,
-    upsample_block,
-)
+    conv_bn_relu_block, sequence_inv_res_bot_block, upsample_block)
 from src.models.lib.config import UNetPPConfig
-from src.models.lib.utils import (  # pylint: disable=wrong-import-position,import-error
-    node_name_func,
-)
+from src.models.lib.utils import \
+    node_name_func  # pylint: disable=wrong-import-position,import-error
 
 
 def base_unet_pp(config: UNetPPConfig):
@@ -221,7 +217,7 @@ def unetpp_mobile_backend(config: UNetPPConfig):
 
     # Input layer for the first node
     model_dict["input"] = keras.Input(shape=config.input_dim, name="x_00_input")
-    model_dict["00"] = layers.Concatenate()(
+    model_dict["00"] = layers.Concatenate(name="x_00_concat")(
         [model_dict["input"], model_dict["input"], model_dict["input"]]
     )
     mobile_backbone = MobileNetV2(
